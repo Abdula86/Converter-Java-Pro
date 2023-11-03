3e5c3460bc3116da476517b3dbf1afbd66bd2bc1
Yurii Chernilov <zavsek.o2@gmail.com>
2023-11-03 20:00:23 +0200

    Add converter mkilometers to miles

--- src/Converter.java
+++ src/Converter.java
@@ -3,8 +3,8 @@ package com.gmail.zavsek_o2;
 import java.util.Scanner;
 
 public class Converter {
-    public static double milesToKilometers(double miles) {
-        return miles * 1.60934;
+    public static double kilometersToMiles(double kilometers) {
+        return kilometers / 1.60934;
     }
 
     public static void main(String[] args) {
