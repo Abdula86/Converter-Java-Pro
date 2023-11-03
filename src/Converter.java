f77542acf27abb5e06cb4bc6884a5a0fa682223d
Yurii Chernilov <zavsek.o2@gmail.com>
2023-11-03 19:56:27 +0200

    Add converter miles to kilometers

---
+++ src/Converter.java
@@ -0,0 +1,13 @@
+package com.gmail.zavsek_o2;
+
+import java.util.Scanner;
+
+public class Converter {
+    public static double milesToKilometers(double miles) {
+        return miles * 1.60934;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
